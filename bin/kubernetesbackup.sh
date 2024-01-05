2627f58d558d257020e1e4b849d3f2728cd3ebe4
KevinB593 <kevinbjones93@hotmail.com>
2024-01-05 17:34:55 +0100

    added pv & pvc and fixed backup script

--- bin/kubernetesbackup.sh
+++ bin/kubernetesbackup.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-source .env
+source /var/dnd/bin/.env
+
 DATE=$(date +%Y%m%d_%H%M%S)
+
 POD_NAME=$(kubectl get pod -l $POD_LABEL -n $NAMESPACE -o jsonpath="{.items[0].metadata.name}")
+if [ $? -ne 0 ]; then
+    echo "Failed to get POD_NAME" >&2
+    exit 1
+fi
+
 kubectl exec $POD_NAME -n $NAMESPACE -- /usr/bin/mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_DIR/db_backup_$DATE.sql
+if [ $? -ne 0 ]; then
+    echo "Database backup command failed" >&2
+    exit 1
+fi
+
+kubectl exec $POD_NAME -n $NAMESPACE -- /usr/bin/mysqldump -u $DB_USER -p$DB_PASSWORD mysql user db > $BACKUP_DIR/db_users_backup_$DATE.sql
+if [ $? -ne 0 ]; then
+    echo "User backup command failed" >&2
+    exit 1
+fi
